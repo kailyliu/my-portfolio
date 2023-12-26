3f4e198f01fa2175b5d4073850534b588d026fc8
kailyl <59887357+kailyl@users.noreply.github.com>
2023-12-25 23:40:11 -0500

    adding speed ! analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/global.css';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
