932feca5fa1e3660bf5f7e7079b1c79468ae6956
Kaily <59887357+kailyliu@users.noreply.github.com>
2024-07-07 00:50:14 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ const IndexPage: React.FC = () => {
       <Head>
         <title>Kaily Liu</title>
         <meta name="description" content="Some things I've made 🤸‍♀️" />
+        <meta name="robots" content="noindex, nofollow" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;700&display=swap" />
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
       </Head>
